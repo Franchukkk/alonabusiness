4283fb5bd64657cae94a774f93282becc6cca18b
Franchukkk <sha471251@gmail.com>
2025-05-16 21:32:43 +0300

    index.html full v1

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,51 +1,34 @@
-const catalogBtn = document.querySelector("#catalog")
-const catalogOpen = document.querySelector("#catalogOpen")
-const arrow = document.querySelector(".catalog span")
-const firmBtn = document.querySelector("#firm-open")
-const firmMenu = document.querySelector("#firms-menu")
+document.addEventListener("DOMContentLoaded", function () {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.burger-navigation');
+    const body = document.body;
 
-catalogBtn.onclick = function() {
-    catalogOpen.classList.toggle("menu-close")
-    arrow.classList.toggle("arrow")
-}
-
-firmBtn.onclick = function() {
-    firmMenu.classList.toggle("menu-open")
-}
-
-document.addEventListener('DOMContentLoaded', function() {
-    var brickButton = document.getElementById('brick');
-    var tileButton = document.getElementById('tile');
-    var cards = document.querySelectorAll('.card');
-  
-    brickButton.addEventListener('click', function() {
-      cards.forEach(function(card) {
-        card.classList.add('brick-card');
-      });
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('active');
+        burger.classList.toggle('open');
+        body.classList.toggle('no-scroll');
     });
-  
-    tileButton.addEventListener('click', function() {
-      cards.forEach(function(card) {
-        card.classList.remove('brick-card');
-      });
+
+    document.querySelectorAll('.burger-navigation a').forEach(link => {
+        link.addEventListener('click', () => {
+            nav.classList.remove('active');
+            burger.classList.remove('open');
+            body.classList.remove('no-scroll');
+        });
+    });
+
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            const targetId = this.getAttribute('href').slice(1);
+            const target = document.getElementById(targetId);
+            if (target) {
+                e.preventDefault();
+                window.scrollTo({
+                    top: target.offsetTop - 80,
+                    behavior: 'smooth'
+                });
+            }
+        });
     });
 });
-  
-
-function myFunction() {
-    // Get the checkbox
-    var checkBox = document.getElementById("myCheck");
-    // Get the output text elements
-    var text1 = document.getElementById("same");
-    var text2 = document.getElementById("different");
-  
-    // If the checkbox is checked, change the font weight of text1
-    if (checkBox.checked == true) {
-        text2.style.fontWeight = "600";
-        text1.style.fontWeight = "normal"; // Reset font weight of text2
-    } else {
-        text1.style.fontWeight = "600";
-        text2.style.fontWeight = "normal"; // Reset font weight of text1
-    }
-}
 
